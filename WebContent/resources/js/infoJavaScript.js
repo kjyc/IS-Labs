d53387f685dc30c212089dfcc21b135363bec220
Kevin Joel Yovera Carlos <joelkevin899@gmail.com>
2021-11-10 12:06:56 +0100

    Laboratorio 4.2

---
+++ WebContent/resources/js/infoJavaScript.js
@@ -0,0 +1,4 @@
+function showMessage(m) {
+	alert(m);
+}
+showMessage("Tus datos:");
--- src/modelo/bean/LoginBean.java
+++ src/modelo/bean/LoginBean.java
@@ -2,6 +2,11 @@ package modelo.bean;
 
 import java.util.Date;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.SelectEvent;
+
 public class LoginBean {
 	private String nombre;
 	private String password;
@@ -32,10 +37,20 @@ public class LoginBean {
 	}
 
 	public String comprobar() {
+		if (nombre.length() != password.length()) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("Error: La longitud del nombre y de la contraseña son diferentes."));
+			return null;
+		}
+
 		if (nombre.equals("pirata")) {
 			return "error";
 		} else {
 			return "ok";
 		}
+	}
+
+	public void onDateSelect(SelectEvent event) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Fecha escogida: " + event.getObject()));
 	}
 }
